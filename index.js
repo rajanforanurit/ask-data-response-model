646ce6c3aa1d0987e187838eb7424c0176d306df
rajanforanurit <rajan.singh@anuritinnovation.com>
2026-05-26 19:01:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express=require('express')
-const cors = require('cors')
+const cors =require('cors')
 const {MongoClient, ObjectId} = require('mongodb')
 const {BlobServiceClient} = require('@azure/storage-blob')
 const pdfParse = require('pdf-parse')
